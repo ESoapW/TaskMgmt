725b2a2dcca150870e3ee9581a4c8033a33c85c7
Eason Wei <easonwei1998@gmail.com>
2022-09-23 01:54:49 +0800

    remove all DS_Store

--- src/main/java/nus/moc/yixwei/health/DatabaseHealthCheck.java
+++ src/main/java/nus/moc/yixwei/health/DatabaseHealthCheck.java
@@ -4,15 +4,15 @@ import com.codahale.metrics.health.HealthCheck;
 import nus.moc.yixwei.db.TaskDAO;
 
 public class DatabaseHealthCheck extends HealthCheck {
-    private final TaskDAO myDAO;
+    private final TaskDAO DAO;
 
     public DatabaseHealthCheck(TaskDAO testDAO) {
-        this.myDAO = testDAO;
+        this.DAO = testDAO;
     }
 
     @Override
     protected Result check() throws Exception {
-        if (myDAO.ping()==1) {
+        if (DAO.ping()==1) {
             return Result.healthy();
         }
         return Result.unhealthy("Can't connect to database");
